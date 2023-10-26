341cea3170f512eead9f6370904a0e63d0803631
Paul T <4277512+pault-tech@users.noreply.github.com>
2023-10-26 14:30:30 -0700

    use go-openapi

--- main.go
+++ main.go
@@ -1,46 +1,22 @@
+package main
 
 import (
-	"fmt"
-	"github.com/pb33f/libopenapi"
-	"io/ioutil"
-	"os"
+    "fmt"
+    "io/ioutil"
+    "os"
+
+    "github.com/go-openapi/loads"
 )
 
 func main() {
+    swaggerDoc, err := loads.Spec(os.Args[1])
+    if err != nil {
+        panic(err)
+    }
 
-	// load an OpenAPI 3 specification from bytes
-	petstore, _ := ioutil.ReadFile(os.Args[0])
-
-	// create a new document from specification bytes
-	document, err := libopenapi.NewDocument(petstore)
-	if err != nil {
-		panic(fmt.Sprintf("cannot create new document: %e", err))
-
-	}
-
-	// because we know this is a v3 spec,
-	// we can build a ready to go model from it.
-	v3Model, errors := document.BuildV3Model()
-
-	// if anything went wrong when building the v3 model,
-	// a slice of errors will be returned
-	if len(errors) > 0 {
-		for i := range errors {
-			fmt.Printf("error: %e\n", errors[i])
-
-		}
-		panic(fmt.Sprintf("cannot create v3 model from "+
-			"document: %d errors reported",
-			len(errors)))
-
-	}
-
-	// get a count of the number of paths and schemas.
-	paths := len(v3Model.Model.Paths.PathItems)
-	schemas := len(v3Model.Model.Components.Schemas)
-
-	// print the number of paths and schemas in the document
-	fmt.Printf("There are %d paths and %d schemas "+
-		"in the document", paths, schemas)
-
+    for path, item := range swaggerDoc.Spec().Paths.Paths {
+        for method, op := range item.Operations() {
+            fmt.Printf("%s %s %s\n", path, method, op.ID)
+        }
+    }
 }
